a8d6d4257725eabb4a6665eb723d8d47b39c086f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-04-27 10:49:11 -0400

    copy into 4 specs

--- cypress/integration/spec-a.js
+++ cypress/integration/spec-a.js
@@ -3,4 +3,7 @@ it('adds todos', () => {
   cy.visit('/')
   cy.get('.new-todo').type('write code{enter}').type('write tests{enter}')
   cy.get('.todo-list li').should('have.length', 2)
+  cy.wait(10000).then(() => {
+    cy.log('done')
+  })
 })
